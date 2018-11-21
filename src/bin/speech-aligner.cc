e6300c400d967cc24189815db219c31e5b0aa1a0
Meixu Song <meixu.asr@gmail.com>
2018-11-21 19:19:09 +0800

    fix cannot find <codecvt>

--- src/bin/speech-aligner.cc
+++ src/bin/speech-aligner.cc
@@ -17,9 +17,7 @@
 // See the Apache 2 License for the specific language governing permissions and
 // limitations under the License.
 
-#include <locale>
-#include <utility>
-#include <codecvt>
+#include <sstream>
 #include <iomanip>
 #include <map>
 #include <fst/util.h>
@@ -118,17 +116,15 @@ bool ReadPhoneSymbol(const string &filename, std::map<int32, string> &id2phn) {
 }
 
 std::wstring s2ws(const std::string& str) {
-  using convert_typeX = std::codecvt_utf8<wchar_t>;
-  std::wstring_convert<convert_typeX, wchar_t> converterX;
-
-  return converterX.from_bytes(str);
+  std::wstringstream wss;
+  wss << str.c_str();
+  return wss.str();
 }
 
 std::string ws2s(const std::wstring& wstr) {
-  using convert_typeX = std::codecvt_utf8<wchar_t>;
-  std::wstring_convert<convert_typeX, wchar_t> converterX;
-
-  return converterX.to_bytes(wstr);
+  std::stringstream ss;
+  ss << wstr.c_str();
+  return ss.str();
 }
 
 bool SegWordFMM(std::map<string, int32> &word2id, const string &sentence,
